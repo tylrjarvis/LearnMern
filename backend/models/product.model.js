0878ab2abe50bf042b31a379b50c76185b261ce0
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-04-23 20:09:10 -0500

    set up model schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String, 
+        required: true,
+
+    },
+}, {
+    timestamps: true,
+});
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,22 @@ dotenv.config(); // Load environment variables from .env
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready");
+app.post("/products", async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:falase, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch(error) {
+        console.log("Error creating product", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
 });
 
 app.listen(5000, () => {
