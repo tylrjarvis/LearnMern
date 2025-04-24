75c3760a62174dd8cf0a7d90f835f77e84710b33
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-04-23 20:34:07 -0500

    add model import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config(); // Load environment variables from .env
 
